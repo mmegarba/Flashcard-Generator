4a78071ca2dc8b27cd69a77f1890c0e1a382a530
mmegarba <micaeljohnmegarbane@gmail.com>
2017-07-28 10:49:05 -0400

    updates

---
+++ BasicCard.js
@@ -0,0 +1,18 @@
+	
+
+var BasicFlashCard = function(front,back){
+
+
+this.front = front;
+this.back = back;
+
+}
+
+
+
+console.log(FlashCard1.front)
+console.log(FlashCard1.back)
+
+
+// exporting our constructor
+module.exports = BasicFlashCard;
---
+++ ClozeCard.js
@@ -0,0 +1,15 @@
+var ClozeCard = function(fullText,cloze){
+
+
+this.cloze = cloze;
+this.fullText = fullText;
+
+
+
+
+
+}
+
+
+
+module.exports = ClozeCard;
